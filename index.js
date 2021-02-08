e2c68f23b911f53a5d3fac3edcbcdd0a18e9b995
technisRahulk <rahulkumaru99@gmail.com>
2021-02-08 15:46:11 +0530

    build added to root directory

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors())
 
-app.use(express.static("public/build"))
+app.use(express.static("build"))
 
 
 app.post('/api/form',(req,res)=>{
